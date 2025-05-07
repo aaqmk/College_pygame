9f5f35952915db55750fd0107d020c067cd08f03
aaqmk <csgoakk254@mail.ru>
2025-05-07 09:30:40 +0500

    Second commit

--- Lesson 4/main.py
+++ Lesson 4/main.py
@@ -1,5 +1,5 @@
 import pygame
-from my_sprite import Platform, Character
+from my_sprite import Platform, Ladder, Character
 
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
@@ -8,18 +8,27 @@ running = True
 
 all_sprites = pygame.sprite.Group()
 platforms = pygame.sprite.Group()
+ladders = pygame.sprite.Group()
 character = None
 
 while running:
     dt = clock.tick(60) / 1000
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                platform = Platform(*event.pos)
-                all_sprites.add(platform)
-                platforms.add(platform)
+                if pygame.key.get_mods() & pygame.KMOD_CTRL:
+                    new_obj = Ladder(*event.pos)
+                    ladders.add(new_obj)
+                else:
+                    new_obj = Platform(*event.pos)
+                    platforms.add(new_obj)
+
+                all_sprites.add(new_obj)
+
             elif event.button == 3:
                 if character:
                     character.rect.center = event.pos
@@ -27,14 +36,19 @@ while running:
                 else:
                     character = Character(*event.pos)
                     all_sprites.add(character)
+
         if event.type == pygame.KEYDOWN and character:
             if event.key == pygame.K_LEFT:
                 character.move_left()
             elif event.key == pygame.K_RIGHT:
                 character.move_right()
+            elif event.key == pygame.K_UP:
+                character.move_up()
+            elif event.key == pygame.K_DOWN:
+                character.move_down()
 
     if character:
-        character.update(platforms, dt)
+        character.update(platforms, ladders, dt)
 
     screen.fill((255, 255, 255))
     all_sprites.draw(screen)
--- Lesson 4/my_sprite.py
+++ Lesson 4/my_sprite.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class GameObject(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, color):
         super().__init__()
@@ -7,29 +8,49 @@ class GameObject(pygame.sprite.Sprite):
         self.image.fill(color)
         self.rect = self.image.get_rect(center=(x, y))
 
+
 class Platform(GameObject):
     def __init__(self, x, y):
         super().__init__(x, y, 50, 10, (128, 128, 128))
+
+
+class Ladder(GameObject):
+    def __init__(self, x, y):
+        super().__init__(x, y, 10, 50, (255, 0, 0))
+
 
 class Character(GameObject):
     def __init__(self, x, y):
         super().__init__(x, y, 20, 20, (0, 0, 255))
         self.velocity = 0
-        self.on_ground = False
+        self.on_ladder = False
 
-    def update(self, platforms, dt):
-        self.velocity += 50 * dt
-        self.rect.y += self.velocity * dt
-        collisions = pygame.sprite.spritecollide(self, platforms, False)
-        if collisions:
-            self.rect.y = collisions[0].rect.top - self.rect.height // 2
+    def update(self, platforms, ladders, dt):
+        ladder_collisions = pygame.sprite.spritecollide(self, ladders, False)
+
+        if ladder_collisions:
+            self.on_ladder = True
             self.velocity = 0
-            self.on_ground = True
         else:
-            self.on_ground = False
+            self.on_ladder = False
+            self.velocity += 50 * dt
+            self.rect.y += self.velocity * dt
+
+            platform_collisions = pygame.sprite.spritecollide(self, platforms, False)
+            if platform_collisions:
+                self.rect.y = platform_collisions[0].rect.top - self.rect.height // 2
+                self.velocity = 0
 
     def move_left(self):
         self.rect.x -= 10
 
     def move_right(self):
         self.rect.x += 10
+
+    def move_up(self):
+        if self.on_ladder:
+            self.rect.y -= 10
+
+    def move_down(self):
+        if self.on_ladder:
+            self.rect.y += 10
